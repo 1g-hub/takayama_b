6f3fdd34050476dc610863573d2b607c8ffbca2f
mtlilium <39237030+mtlilium@users.noreply.github.com>
2020-12-17 23:57:07 +0900

    pptx

--- src/const.py
+++ src/const.py
@@ -2,3 +2,8 @@
 class const():
     def __init__(self):
         self.miria = 100
+
+
+a = const()
+
+print(a.miria)
--- src/make_subword.py
+++ src/make_subword.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import mojimoji
 
 from transformers import BertTokenizer
 
@@ -24,25 +25,28 @@ print(bert_tokenizer.tokenize(s))
 print(bert_tokenizer.convert_tokens_to_ids(s))
 print(bert_tokenizer.batch_encode_plus(s, pad_to_max_length=False, add_special_tokens=True, is_split_into_words=True))
 print(bert_tokenizer.batch_encode_plus(s, pad_to_max_length=False, add_special_tokens=True, is_split_into_words=False))
-# for touch_name in TOUCH_NAME_ENG:
-#     data = pd.read_csv(
-#         '../new_dataset/' + touch_name + '_modified_aug.csv',
-#         index_col=0,
-#         header=0,
-#         dtype={'id': int, 'original': bool, 'inner': bool, 'kakimoji': bool, 'self_anotated': bool,
-#                'alter_emotion': str},
-#     )
-#
-#     for index, row in data.iterrows():
-#         wakati = data.at[index, 'wakati'].split(" ")
-#         s_subword = []
-#         for w in wakati:
-#             if w == '!':
-#                 w = ''
-#             word_after = bert_tokenizer.tokenize(w)
-#             for word in word_after:
-#                 if word == '[UNK]':
-#                     word = ''.join(w)
-#                 s_subword.append(word)
-#         data.at[index, 'subword'] = ' '.join(s_subword)
-#     data.to_csv('../new_dataset/' + touch_name + '_modified_aug_add_sub.csv', encoding='utf_8_sig')
+for touch_name in TOUCH_NAME_ENG:
+    data = pd.read_csv(
+        '../new_dataset/' + touch_name + '_modified_aug.csv',
+        index_col=0,
+        header=0,
+        dtype={'id': int, 'original': bool, 'inner': bool, 'kakimoji': bool, 'self_anotated': bool,
+               'alter_emotion': str},
+    )
+
+    for index, row in data.iterrows():
+        wakati = data.at[index, 'wakati'].split(" ")
+        s_subword = []
+        for w in wakati:
+            if w == '!':
+                w = '！'
+            elif w == '?':
+                w = '？'
+            w = mojimoji.han_to_zen(w)
+            word_after = bert_tokenizer.tokenize(w)
+            for word in word_after:
+                if word == '[UNK]':
+                    word = ''.join(w)
+                s_subword.append(word)
+        data.at[index, 'subword'] = ' '.join(s_subword)
+    data.to_csv('../new_dataset/' + touch_name + '_modified_aug_add_sub.csv', encoding='utf_8_sig')
